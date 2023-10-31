ac352349be78a9b4be2d0a3a7d5adf6ef911425e
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-31 11:54:16 +0100

    update main

--- aantalsecoden.mjs
+++ aantalsecoden.mjs
@@ -2,3 +2,13 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+let dagen = parseFloat(await userInput.question('Geef het aantal dagen: '));
+let uren = parseFloat(await userInput.question('Geef het aantal uren: '));
+let minuten = parseFloat(await userInput.question('Geef het aantal minuten: '));
+let seconden = parseFloat(await userInput.question('Geef het aantal seconden: '));
+
+let totaalSeconden = (dagen * 24 * 60 * 60) + (uren * 60 * 60) + (minuten * 60) + seconden;
+
+console.log('Het totaal aantal seconden is: ' + totaalSeconden);
+
+process.exit();
