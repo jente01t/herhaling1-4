1ae9453fd86076c934c2d2548feeb3c42a8345c8
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-31 17:33:45 +0100

    Update sterrenrechthoek.mjs

--- sterrenrechthoek.mjs
+++ sterrenrechthoek.mjs
@@ -2,3 +2,23 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+let breedte = parseFloat(await userInput.question('Geef de breedte van de rechthoek: '));
+let hoogte = parseFloat(await userInput.question('Geef de hoogte van de rechthoek: '));
+
+for(let i = 1; i <= hoogte; i++){
+    let rij = '';
+    if (i == 1 || i == hoogte) {
+        for (let j = 1; j <= breedte; j++) {
+            rij += '*';
+        }
+    } else {
+        rij += '*';
+        for(let j = 1; j <= breedte - 2; j++) {
+            rij += ' ';
+        }
+        rij +='*'
+    }
+    console.log(rij);
+}
+
+process.exit();
