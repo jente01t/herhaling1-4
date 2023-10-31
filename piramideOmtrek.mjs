80142ad56ca08cd8458e7606146f9bc78c08d9c5
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-31 17:42:07 +0100

    Update piramideOmtrek.mjs

--- piramideOmtrek.mjs
+++ piramideOmtrek.mjs
@@ -1,4 +1,27 @@
 import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
 
+let hoogte = parseFloat(await userInput.question('Geef de hoogte van de piramide: '));
+
+let outline = false
+
+if (outline) {
+  for (let i = 1; i <= hoogte; i++) {
+    let rij = '';
+    for (let j = 2; j <= i; j++) {
+      rij += ' ';
+    } rij += '*';
+    console.log(rij)
+  }
+} else {
+  for (let i = 0; i <= hoogte - 1; i++) {
+    let rij = '';
+    for (let j = 1; j <= i; j++) {
+      rij += '*';
+    }
+    console.log(rij)
+  }
+}
+
+process.exit();
