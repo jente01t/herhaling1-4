336d87643942a47896684a908d87f7a454c62fce
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-11-08 10:27:37 +0100

    Update omzettenPunt.mjs

--- omzettenPunt.mjs
+++ omzettenPunt.mjs
@@ -2,8 +2,6 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-
-
 async function omzetten () {
     let punt = parseFloat(await userInput.question('Geef het punt: '));
     let maxPunt = parseFloat(await userInput.question('Geef het maximale punt dat je kunt bereiken: '));
