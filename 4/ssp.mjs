c7b4e2eaebca1312b6812afef9a040591610a279
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-11-11 14:48:09 +0100

    update main

---
+++ 4/ssp.mjs
@@ -0,0 +1,42 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let user = 0;
+let computer = 0;
+
+while (user <= 3) {
+    await keuze (user, computer);
+}
+
+
+
+async function keuze (user, computer) {
+    let keuzeUser = await userInput.question('SSP? ');
+    let choices = ['steen', 'papier', 'schaar'];
+    let keuzeComputer = choices[Math.floor(Math.random()*choices.length)];
+
+    if (keuzeUser == 'schaar' && keuzeComputer == 'papier' || 
+    keuzeUser == 'steen' && keuzeComputer == 'schaar' ||
+    keuzeUser == 'papier' && keuzeComputer == 'steen') {
+        user++;
+    } else {
+        computer++;
+    }
+    winaar (user, computer)
+}
+
+async function winaar (user, computer) {
+    console.log(`Gebruiker: ${user} - Computer: ${computer}`)
+    if (user == 3 || computer == 3) {
+        if (user == 3) {
+            console.log('Gebruiker heeft gewonnen.')
+        } else {
+            console.log('Computer heeft gewonnen.')
+        }
+    } else {
+        await keuze(user, computer)
+    }
+}
+
+process.exit();
