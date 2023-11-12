7f1e632b926405b6e519d4adb0ee245c6697d46e
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-11-12 17:12:37 +0100

    Create reis.mjs

---
+++ GPT/reis.mjs
@@ -0,0 +1,11 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let reizen = [
+    ["Ibiza", [300, 400, 245, 780, 423]],
+    ["Mallorca", [500, 550, 600, 720]],
+    ["Madeira", [650, 750, 910, 600, 620]],
+    ["Kreta", [530, 590, 610, 490, 495]]
+]
+
